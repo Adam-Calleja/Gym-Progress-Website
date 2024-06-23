09c66cfd22c18adf24df8f66927c2eccb0ecb70e
Adam-Calleja <adam.calleja@student.manchester.ac.uk>
2024-06-23 18:42:10 +0100

    Remove hardcoded URLs in templates
    Removed the hardoced URLs from the base.html template.

--- gymProgressWebsite/core/urls.py
+++ gymProgressWebsite/core/urls.py
@@ -3,8 +3,8 @@ from core.views import Index, Account, LogIn, Register
 
 
 urlpatterns = [
-    path("", Index.as_view()),
-    path("login/", LogIn.as_view()),
-    path("account/", Account.as_view()),
-    path("register/", Register.as_view()),
+    path("", Index.as_view(), name="index"),
+    path("login/", LogIn.as_view(), name="login"),
+    path("account/", Account.as_view(), name="account"),
+    path("register/", Register.as_view(), name="register"),
 ]
